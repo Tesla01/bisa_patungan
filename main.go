af2fb8d23f3b55ece42ba74db85ab9a678420a7b
Arif Sulaiman <arif.sulaiman720@gmail.com>
2023-02-12 20:17:59 +0700

    Set Up User Entity

--- main.go
+++ main.go
@@ -5,16 +5,30 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"tesla01/bisa_patungan/user"
 )
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:@tcp(127.0.0.1:3306)/golang_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection Success")
+
+	var users []user.User
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find(&users)
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Printf("%s-%s\n", user.Name, user.Email)
+		fmt.Println("========================")
+	}
 }
