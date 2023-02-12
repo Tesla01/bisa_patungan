501e61d046fd02b8db94ab6d2903f9293f9068f1
Arif Sulaiman <arif.sulaiman720@gmail.com>
2023-02-12 22:04:00 +0700

    Test Gin

--- main.go
+++ main.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"tesla01/bisa_patungan/user"
 )
 
 func main() {
+	//// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	//dsn := "root:@tcp(127.0.0.1:3306)/golang_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//
+	//if err != nil {
+	//	log.Fatal(err.Error())
+	//}
+	//
+	//fmt.Println("Connection Success")
+	//
+	//var users []user.User
+	//length := len(users)
+	//fmt.Println(length)
+	//
+	//db.Find(&users)
+	//length = len(users)
+	//fmt.Println(length)
+	//
+	//for _, user := range users {
+	//	fmt.Printf("%s-%s\n", user.Name, user.Email)
+	//	fmt.Println("========================")
+	//}
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:@tcp(127.0.0.1:3306)/golang_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -20,15 +50,8 @@ func main() {
 	fmt.Println("Connection Success")
 
 	var users []user.User
-	length := len(users)
-	fmt.Println(length)
 
 	db.Find(&users)
-	length = len(users)
-	fmt.Println(length)
 
-	for _, user := range users {
-		fmt.Printf("%s-%s\n", user.Name, user.Email)
-		fmt.Println("========================")
-	}
+	c.JSON(http.StatusOK, users)
 }
