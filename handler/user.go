1c5965e5c011c77829e2e87731cb1be890437ef7
Arif Sulaiman <arif.sulaiman720@gmail.com>
2023-02-17 23:12:17 +0700

    Add User Validation

--- handler/user.go
+++ handler/user.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 	"tesla01/bisa_patungan/helper"
 	"tesla01/bisa_patungan/user"
@@ -25,12 +26,26 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	err := c.ShouldBindJSON(&input)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		var errors []string
+
+		for _, e := range err.(validator.ValidationErrors) {
+			errors = append(errors, e.Error())
+		}
+
+		// Gin Mapping
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
 	}
 
 	newUser, err := h.userService.RegisterUser(input)
+
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 
 	user := user.FormatUser(newUser, "tokentoken")
